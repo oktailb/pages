b6d8e805fff7e0f556ca7c1c49d35f97f9ae8111
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2025-06-03 13:14:12 +0900

    Create Grid.js

---
+++ Grid.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Cell from './Cell';
+
+const Grid = ({ rows, cols, gridData }) => {
+  return (
+    <div className="grid">
+      {gridData.map((row, rowIndex) => (
+        <div key={rowIndex} className="row">
+          {row.map((cellValue, colIndex) => (
+            <Cell
+              key={colIndex}
+              row={rowIndex}
+              col={colIndex}
+              value={cellValue}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Grid;
